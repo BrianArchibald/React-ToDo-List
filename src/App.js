5c3eac4f4e10938480bddf55cc35438457368b30
BrianArchibald <brianallenarchibald@gmail.com>
2019-02-03 09:38:43 -0600

    added todo item comp, passing props to children

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import Todos from "./components/Todos";
-import "./App.css";
 
 class App extends Component {
   state = {
@@ -23,10 +22,14 @@ class App extends Component {
     ]
   };
 
+  markComplete = id => {
+    console.log(id);
+  };
+
   render() {
     return (
       <div className="App">
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} markComplete={this.markComplete} />
       </div>
     );
   }
